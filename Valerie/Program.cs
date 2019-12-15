25f5aca4de3e7f8f36d5b9f05d5abd8525289fa6
Hrvoje Zeman <hrvoje.zeman@student.mev.hr>
2019-12-15 08:22:43 -0800

    Added support for killing a process

--- Valerie/Program.cs
+++ Valerie/Program.cs
@@ -7,51 +7,49 @@ namespace Valerie
     {
         public static void Main(string[] args)
         {
-            string ulaz = null;
-            string[] dijelovi = null;
-            string naredba = null;
-            string prvi_uvjet = null;
-            string drugi_uvjet = null;
+            string input = null;
+            string[] Fragments = null;
+            string Command = null;
+            string FirstCodition = null;
+            string SecondCondition = null;
             string zadnji_rezultati = null;
 
             int maksimalna_udaljenost = 30;
             int duzina_teksta = 0;
 
-            while (true)
+            if (args.Length > 0)
             {
-                ulaz = Console.ReadLine();
+                Fragments = args;
+                Command = Fragments[0];
 
-                dijelovi = ulaz.Split(' ');
-                naredba = dijelovi[0];
+                Process[] ListOfProcesses = Process.GetProcesses();
 
-                Process[] popis_procesa = Process.GetProcesses();
-
-                // ispisi_procese @regName chrome
-                if (naredba == "ispisi_procese")
+                // ListProcess @regName chrome
+                if (Command == "ListProcess")
                 {
                     try
                     {
-                        prvi_uvjet = dijelovi[1];
+                        FirstCodition = Fragments[1];
                     }
                     catch (IndexOutOfRangeException)
                     {
-                        prvi_uvjet = "";
+                        FirstCodition = "";
                     }
 
                     try
                     {
-                        drugi_uvjet = dijelovi[2];
+                        SecondCondition = Fragments[2];
                     }
                     catch (IndexOutOfRangeException)
                     {
-                        drugi_uvjet = "";
+                        SecondCondition = "";
                     }
 
-                    if (prvi_uvjet == "@regName")
+                    if (FirstCodition == "@regName")
                     {
-                        foreach (Process trenutni_proces in popis_procesa)
+                        foreach (Process trenutni_proces in ListOfProcesses)
                         {
-                            if (drugi_uvjet == trenutni_proces.ProcessName.ToString())
+                            if (SecondCondition == trenutni_proces.ProcessName.ToString())
                             {
                                 Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
                                     trenutni_proces.ProcessName, trenutni_proces.Id));
@@ -60,12 +58,12 @@ namespace Valerie
                         }
                     }
 
-                    // ispisi_procese @!regName chrome
-                    else if (prvi_uvjet == "@!regName")
+                    // ListProcess @!regName chrome
+                    else if (FirstCodition == "@!regName")
                     {
-                        foreach (Process trenutni_proces in popis_procesa)
+                        foreach (Process trenutni_proces in ListOfProcesses)
                         {
-                            if (drugi_uvjet != trenutni_proces.ProcessName.ToString())
+                            if (SecondCondition != trenutni_proces.ProcessName.ToString())
                             {
                                 Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
                                     trenutni_proces.ProcessName, trenutni_proces.Id));
@@ -73,23 +71,23 @@ namespace Valerie
                         }
                     }
 
-                    else if (prvi_uvjet == "@regID")
+                    else if (FirstCodition == "@regID")
                     {
-                        foreach (Process trenutni_proces in popis_procesa)
+                        foreach (Process CurrentProcess in ListOfProcesses)
                         {
-                            if (drugi_uvjet.ToString() == trenutni_proces.Id.ToString())
+                            if (SecondCondition.ToString() == CurrentProcess.Id.ToString())
                             {
                                 Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
-                                    trenutni_proces.ProcessName, trenutni_proces.Id));
+                                    CurrentProcess.ProcessName, CurrentProcess.Id));
                             }
                         }
                     }
-                    // ispisi_procese @!regID 5516
-                    else if (prvi_uvjet == "@!regID")
+                    // ListProcess @!regID 5516
+                    else if (FirstCodition == "@!regID")
                     {
-                        foreach (Process trenutni_proces in popis_procesa)
+                        foreach (Process trenutni_proces in ListOfProcesses)
                         {
-                            if (drugi_uvjet.ToString() != trenutni_proces.Id.ToString())
+                            if (SecondCondition.ToString() != trenutni_proces.Id.ToString())
                             {
                                 Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
                                     trenutni_proces.ProcessName, trenutni_proces.Id));
@@ -98,9 +96,150 @@ namespace Valerie
                     }
                 }
 
-                else if (naredba == "ispisi_zadnje")
+                else if (Command == "ispisi_zadnje")
                 {
                     Console.WriteLine(zadnji_rezultati);
+                }
+
+                // else if (naredba == "spremi")
+                // {
+                //     Console.WriteLine(zadnji_rezultati);
+                // }
+            }
+
+            else
+            {
+                while (true)
+                {
+                    input = Console.ReadLine();
+
+                    if (input != null) Fragments = input.Split(' ');
+                    Command = Fragments[0];
+
+                    Process[] ListOfProcesses = Process.GetProcesses();
+
+                    // ListProcess @regName chrome
+                    if (Command == "ListProcess")
+                    {
+                        try
+                        {
+                            FirstCodition = Fragments[1];
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            FirstCodition = "";
+                        }
+
+                        try
+                        {
+                            SecondCondition = Fragments[2];
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            SecondCondition = "";
+                        }
+
+                        if (FirstCodition == "@regName")
+                        {
+                            foreach (Process trenutni_proces in ListOfProcesses)
+                            {
+                                if (SecondCondition == trenutni_proces.ProcessName.ToString())
+                                {
+                                    Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
+                                        trenutni_proces.ProcessName, trenutni_proces.Id));
+                                    // Console.WriteLine("Process: {0} {1}", trenutni_proces.ProcessName, "ID:" + trenutni_proces.Id);
+                                }
+                            }
+                        }
+
+                        // ListProcess @!regName chrome
+                        else if (FirstCodition == "@!regName")
+                        {
+                            foreach (Process trenutni_proces in ListOfProcesses)
+                            {
+                                if (SecondCondition != trenutni_proces.ProcessName.ToString())
+                                {
+                                    Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
+                                        trenutni_proces.ProcessName, trenutni_proces.Id));
+                                }
+                            }
+                        }
+
+                        else if (FirstCodition == "@regID")
+                        {
+                            foreach (Process CurrentProcess in ListOfProcesses)
+                            {
+                                if (SecondCondition.ToString() == CurrentProcess.Id.ToString())
+                                {
+                                    Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
+                                        CurrentProcess.ProcessName, CurrentProcess.Id));
+                                }
+                            }
+                        }
+                        // ListProcess @!regID 5516
+                        else if (FirstCodition == "@!regID")
+                        {
+                            foreach (Process trenutni_proces in ListOfProcesses)
+                            {
+                                if (SecondCondition.ToString() != trenutni_proces.Id.ToString())
+                                {
+                                    Console.WriteLine(String.Format("Process: {0,-60} ID: {1,-60}",
+                                        trenutni_proces.ProcessName, trenutni_proces.Id));
+                                }
+                            }
+                        }
+                    }
+
+                    else if (Command == "Kill")
+                    {
+                        try
+                        {
+                            FirstCodition = Fragments[1];
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            FirstCodition = "";
+                        }
+
+                        try
+                        {
+                            SecondCondition = Fragments[2];
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            SecondCondition = "";
+                        }
+
+                        // Kill @regName notepad
+                        if (FirstCodition == "@regName")
+                        {
+                            int counter = 0; // counter for number of killed processes
+
+                            foreach (Process CurrentProcess in ListOfProcesses)
+                            {
+                                if (SecondCondition == CurrentProcess.ProcessName.ToString())
+                                {
+                                    CurrentProcess.Kill();
+                                    counter++;
+                                }
+                            }
+
+                            if (counter == 1)
+                            {
+                                Console.WriteLine("Command executed successfully, {0} process killed.", counter);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Command executed successfully, {0} processes killed.", counter);
+                            }
+                        }
+                    }
+
+
+                    else if (Command == "ispisi_zadnje")
+                    {
+                        Console.WriteLine(zadnji_rezultati);
+                    }
                 }
 
                 // else if (naredba == "spremi")
